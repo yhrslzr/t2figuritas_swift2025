f0834ad52eac9a49fc21f79129951470bdd7a238
Yahir Salazar Ortiz <yahir.salazarortiz0113@gmail.com>
2025-09-05 17:45:16 -0600

    Initial Commit

---
+++ t2figuritas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  t2figuritas
+//
+//  Created by WIN603 on 05/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ t2figuritas/t2figuritasApp.swift
@@ -0,0 +1,17 @@
+//
+//  t2figuritasApp.swift
+//  t2figuritas
+//
+//  Created by WIN603 on 05/09/25.
+//
+
+import SwiftUI
+
+@main
+struct t2figuritasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
